d63fa4b28e495f91148c35790ba2ca848dd59539
Littlehhh <wanghuichn@yahoo.com>
2016-12-28 21:34:16 +0800

    Update

--- HM1/Fresnel.m
+++ HM1/Fresnel.m
@@ -8,7 +8,7 @@ a=theta*pi/180;   %角度化为弧度
 %temp(temp<0) = 0;
 %b = sqrt(temp);
 b = asin(n1/n2*sin(a));
-b(b>=1)=1;
+% b(b>=1)=1;
 b = cos(b);
 rp =(n2/u2*cos(a)-n1/u1*b)./...
     (n2/u2*cos(a)+n1/u1*b);   %p分量振幅反射系数
@@ -16,12 +16,22 @@ rs = (n1/u1*cos(a)-n2/u2*b)./...
     (n1/u1*cos(a)+n2/u2*b);   %s分量振幅反射系数
 tp = 2*n1/u1*cos(a)./(n2/u2*cos(a)+n1/u1*b);%p分量振幅透射系数
 ts = 2*n1/u1*cos(a)./(n1/u1*cos(a)+n2/u2*b);%s分量振幅透射系数
-
+if n1>n2
+    [max,index] = findpeaks(real(rp));
+    rp(index:end) = max;
+    [max,index] = findpeaks(real(rs));
+    rs(index:end) = max;
+    [max,index] = findpeaks(real(tp));
+    tp(index:end) = max;
+    [max,index] = findpeaks(real(ts));
+    ts(index:end) = max;
+end
 PS = rs.^2;
 PP = rp.^2;
 TS = 1-PS;
 TP = 1-PP;
 PN = (PS+PP)./2;  %反射比
 TN = 1-PN;        %折射比
+
 
 
